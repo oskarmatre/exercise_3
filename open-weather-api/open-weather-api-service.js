b159aab25441b22b1bbe8d64d8f024491beaa73e
oskarmatre <69513601+oskarmatre@users.noreply.github.com>
2023-03-07 11:59:57 +0100

    /:city endpoint is working
    Co-authored-by: Tobias Thrane <MollersThrane@users.noreply.github.com>
    Co-authored-by: Andreas Haugli <alhaugli@users.noreply.github.com>

--- open-weather-api/open-weather-api-service.js
+++ open-weather-api/open-weather-api-service.js
@@ -1,19 +1,23 @@
+const axios = require("axios");
 /**
  * This class is used retrieve weather data for a given city, by calling the Open Weather API.
  * See here for the Open Weather API documentation - https://openweathermap.org/current.
  */
 class OpenWeatherAPIService {
+  // The API key that is needed to successfully authenticate when calling the Open Weather API.
+  // if this does not work, register one yourself using the instruction here: https://openweathermap.org/appid
+  static apiKey = "480bdd4226f844eff4cede53daf507c7";
 
-    // The API key that is needed to successfully authenticate when calling the Open Weather API. 
-    // if this does not work, register one yourself using the instruction here: https://openweathermap.org/appid
-    static apiKey = "c8da4ee199183df66c17fe7a2b6bf6da";
-
-    /**
-     * Retrieves the weather data for the given city by calling the Open Weather API.
-    */
-    async getWeather(cityName) {
-        //TODO: Implement...
-    }
+  /**
+   * Retrieves the weather data for the given city by calling the Open Weather API.
+   */
+  async getWeather(cityName) {
+    //TODO: Implement...
+    const weather = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OpenWeatherAPIService.apiKey}`
+    );
+    return weather.data;
+  }
 }
 
 module.exports = OpenWeatherAPIService;
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -1,12 +1,30 @@
 import express from 'express'
+import OpenWeatherAPIService from "../../open-weather-api/open-weather-api-service.js";
 const router = express.Router();
 
-router.get('/:city', function(req, res) {
+router.get("/:city", function (req, res) {
   //TODO Implement
+  const openWeatherAPIServiceInstance = new OpenWeatherAPIService();
+  openWeatherAPIServiceInstance
+    .getWeather(req.params.city)
+    .then((weather) => {
+      const weather2 = {
+        weatherDescription: weather.weather[0].description,
+        temperature: Math.round(weather.main.temp_min - 273.15),
+        cityName: weather.name,
+      };
+
+      res.send(weather2);
+    })
+    .catch((err) => {
+      res.status(404).send("404 Not found");
+    });
 });
 
-router.get('/', function(req, res) {
+router.get("/", function (req, res) {
   //TODO Implement
+  try {
+  } catch (error) {}
 });
 
 
