06ad332db9093f2c9470b33a3c3a3194e4b28524
oskarmatre <69513601+oskarmatre@users.noreply.github.com>
2023-03-11 14:57:34 +0100

    all tests are passing now

--- open-weather-api/open-weather-api-service.js
+++ open-weather-api/open-weather-api-service.js
@@ -1,4 +1,7 @@
 const axios = require("axios");
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({ stdTTL: 5 * 60, checkperiod: 120 });
+
 /**
  * This class is used retrieve weather data for a given city, by calling the Open Weather API.
  * See here for the Open Weather API documentation - https://openweathermap.org/current.
@@ -12,11 +15,32 @@ class OpenWeatherAPIService {
    * Retrieves the weather data for the given city by calling the Open Weather API.
    */
   async getWeather(cityName) {
-    //TODO: Implement...
+    const cachedWeather = myCache.get(cityName);
+    if (cachedWeather) {
+      return cachedWeather;
+    }
     const weather = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${OpenWeatherAPIService.apiKey}`
     );
+    myCache.set(cityName, weather.data);
+    if (myCache.stats.keys > 5) {
+      myCache.del(myCache.keys()[0]);
+    }
     return weather.data;
+  }
+
+  async getCachedWeather(ammount) {
+    const cachedWeather = [];
+    let i = 0;
+    let cities = myCache.keys();
+    cities.forEach((city) => {
+      if (cities.length - i > ammount) {
+        i++;
+        return;
+      }
+      cachedWeather.unshift(myCache.get(city));
+    });
+    return cachedWeather;
   }
 }
 
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -3,18 +3,15 @@ import OpenWeatherAPIService from "../../open-weather-api/open-weather-api-servi
 const router = express.Router();
 
 router.get("/:city", function (req, res) {
-  //TODO Implement
   const openWeatherAPIServiceInstance = new OpenWeatherAPIService();
   openWeatherAPIServiceInstance
     .getWeather(req.params.city)
     .then((weather) => {
-      const weather2 = {
+      res.send({
         weatherDescription: weather.weather[0].description,
         temperature: Math.round(weather.main.temp_min - 273.15),
         cityName: weather.name,
-      };
-
-      res.send(weather2);
+      });
     })
     .catch((err) => {
       res.status(404).send("404 Not found");
@@ -22,9 +19,24 @@ router.get("/:city", function (req, res) {
 });
 
 router.get("/", function (req, res) {
-  //TODO Implement
-  try {
-  } catch (error) {}
+  const openWeatherAPIServiceInstance = new OpenWeatherAPIService();
+  openWeatherAPIServiceInstance
+    .getCachedWeather(req.query.max)
+    .then((weather) => {
+      let cityList = [];
+      for (const [key, city] of Object.entries(weather)) {
+        cityList.push({
+          weatherDescription: city.weather[0].description,
+          temperature: Math.round(city.main.temp_min - 273.15),
+          cityName: city.name,
+        });
+      }
+      res.send(cityList);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send("404 Not found");
+    });
 });
 
 
